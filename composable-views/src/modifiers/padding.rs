d929ccf65acb5978dce9071ec83887a3c1bf92e9
Bryan Woods <woods.bryan@gmail.com>
2024-11-17 21:09:16 -0800

    Padding offsets

--- composable-views/src/modifiers/padding.rs
+++ composable-views/src/modifiers/padding.rs
@@ -17,7 +17,7 @@ impl<V: View> View for Padding<V> {
 
     #[inline(always)]
     fn event(&self, event: Event, offset: Point, bounds: Bounds) {
-        self.view.event(event, offset, bounds)
+        self.view.event(event, offset, bounds.inner_box(self.offsets))
     }
 
     #[inline]
